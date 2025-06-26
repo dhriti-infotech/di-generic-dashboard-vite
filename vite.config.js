a06b12c74ee5a8658178ac306319c3b3502b8f14
dhriti-infotech <infotech.dhriti@gmail.com>
2025-06-27 01:58:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/digeneric-dashboard-vite/',
+  base: '/di-generic-dashboard-vite/',
   plugins: [react()],
   build: {
     rollupOptions: {
